d2ae386a8d9f38ee3a86bf0f9858490a330f53a0
Javier Díaz <javier.diaz@iohk.io>
2022-12-10 21:29:20 -0300

    Simplify `(simp only:, fastforce)` to just `simp`

--- src/Network_Equivalences-Communication.thy
+++ src/Network_Equivalences-Communication.thy
@@ -698,12 +698,12 @@ using assms unfolding duploss_def proof cases
   case parallel_left_io
   with losing show ?thesis
     using parallel_associativity and adapted_after_parallel
-    by (elim transition_from_loss, simp only:, fastforce)
+    by (elim transition_from_loss, simp)
 next
   case parallel_right_io
   with duplicating show ?thesis
     using parallel_left_commutativity and adapted_after_parallel
-    by (elim transition_from_duplication, simp only:, fastforce)
+    by (elim transition_from_duplication, simp)
 qed (fastforce elim: transition_from_repeated_receive)+
 
 lemma duploss_idempotency [thorn_simps]:
