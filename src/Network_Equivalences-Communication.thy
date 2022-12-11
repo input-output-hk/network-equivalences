4c51b5d5f8f6691b0144b224336e7796c18cd399
Javier Díaz <javier.diaz@iohk.io>
2022-12-10 21:34:17 -0300

    Simplify repeated `fastforce` to just `auto`

--- src/Network_Equivalences-Communication.thy
+++ src/Network_Equivalences-Communication.thy
@@ -704,7 +704,7 @@ next
   with duplicating show ?thesis
     using parallel_left_commutativity and adapted_after_parallel
     by (elim transition_from_duplication, simp)
-qed (fastforce elim: transition_from_repeated_receive)+
+qed (auto elim: transition_from_repeated_receive)
 
 lemma duploss_idempotency [thorn_simps]:
   shows "\<currency>\<^sup>* A \<parallel> \<currency>\<^sup>* A \<sim>\<^sub>s \<currency>\<^sup>* A"
